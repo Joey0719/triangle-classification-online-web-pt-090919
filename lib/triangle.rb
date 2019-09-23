a00d0a3b7e4c8cfb750e6dbfdce5a4be9e0c205f
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-23 22:00:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
-  # write code here
-end
+  
+  attr_accessor :x, :y, :z,
+  :equilateral, :isosceles, :scalene
+  
+  def initialize(x, y, z)
+    @x = x 
+    @y = y 
+    @z = z 
+  end
+  
+  
