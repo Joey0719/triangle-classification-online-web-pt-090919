ab0c36054b91295ca9d0c16dc89fb4a85eabd7df
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-09-23 22:06:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,4 +9,20 @@ class Triangle
     @z = z 
   end
   
-  
+  def kind 
+    if x == 0 || y == 0 || z == 0 
+      raise TriangleError
+    elsif x + y <= z || x + z <= y || y + z <= x 
+      raise TriangleError
+    elsif x == y && y == z 
+      kind = :equilateral
+    elsif x == y && x != z || y == z && y != x || x == z && x != y 
+      kind = :isosceles
+    else 
+      kind = :scalene 
+    end 
+  end
+end 
+
+class TriangleError < StandardError
+end
